0bc6efd8e55454d771845cdd2b93cd433b0f5253
kelseamc <kelsea.mcall@gmail.com>
2021-05-05 16:26:10 -0400

    deploy

--- src/Components/DetailModal.js
+++ src/Components/DetailModal.js
@@ -6,7 +6,7 @@ function DetailModal({show, movie, closeModal, noms, handleNom, handleRemove}) {
 
     useEffect(() => {
         let isMounted = true;
-        fetch(`https://www.omdbapi.com/?i=${movie.imdbID}&type=movie&apikey=${REACT_APP_OMDB_API_KEY}`)
+        fetch(`https://www.omdbapi.com/?i=${movie.imdbID}&type=movie&apikey=${process.env.REACT_APP_OMDB_API_KEY}`)
         .then((r) => r.json())
         .then(data => {
             if (isMounted) setDetails(data)
